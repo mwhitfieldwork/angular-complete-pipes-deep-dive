58e532f6be7d2a101422dfb3bc62b3be6dde94ae
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-11-26 11:49:09 -0600

    Add more pipe stuff

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -2,15 +2,55 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
     name: 'temperature',
-    standalone: true
+    standalone: true,
+    pure:false //disallow caching so that you dont have to replace the array
+                // it might have a performance impact because it mihgt be called a lot
 })
 
 export class TemperaturePipe implements PipeTransform{
 // the PipeTransform interface requires the transform method
 
  //without the transform method, the pipe won't work
- transform(value: string | number , ...args: any[]): any {
+ //Example: {{ currentTemperaturs.newYork | temperature }} 
+ //the value is the currentTemperaturs.newYork
+ transform(
+    value: string | number | null,
+     inputType:'cel'|'fah',
+      outputType?:'cel'|'fah'): any {
+
+     if(!value) {
+        return value;
+     }
+
     //it must return the transformed value
-    return value + 'value transformed';
+    let val:number;
+
+if (typeof value === 'string') {
+    val = parseFloat(value);
+    } else {
+        val = value;
+    }
+
+    let outputTemp:number;
+
+    if(inputType === 'cel' && outputType === 'fah') {
+         outputTemp= val * (9/5) + 32;
+    }else if(inputType === 'fah' && outputType === 'cel') {
+        outputTemp= (val - 32) * (5/9);
+    }else{
+        outputTemp = val;
+    }
+
+     outputTemp= val * (9/5) + 32;
+
+     let symbol: 'C' | 'F';
+
+     if(!outputType){
+        symbol=inputType === 'cel' ? 'C' : 'F';
+     }else{
+        symbol=outputType === 'cel' ? 'C' : 'F';
+     }
+
+    return `${outputTemp.toFixed(2)} ${symbol}`;
  }
 }
